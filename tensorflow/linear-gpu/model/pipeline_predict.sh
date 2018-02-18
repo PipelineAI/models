271d553386aa75e8cf7abf592deb86be895e4822
cfregly <chris@fregly.com>
2018-02-17 17:16:17 -0800

    added _predict.sh and _train.sh

--- tensorflow/linear-gpu/model/pipeline_predict.sh
+++ tensorflow/linear-gpu/model/pipeline_predict.sh
@@ -1,7 +1,8 @@
-PIPELINE_MODEL_RUNTIME=tfserving \
-PIPELINE_MODEL_TYPE=tensorflow \
 PIPELINE_MODEL_NAME=linear \
-PIPELINE_MODEL_TAG=cpu \
-PIPELINE_INPUT_PATH=../input/ \
-PIPELINE_OUTPUT_PATH=../output/ \
+PIPELINE_MODEL_TAG=gpu \
+PIPELINE_MODEL_TYPE=tensorflow \
+PIPELINE_MODEL_RUNTIME=tfserving \
+PIPELINE_MODEL_CHIP=gpu \
+PIPELINE_INPUT_PATH=../input \
+PIPELINE_OUTPUT_PATH=../output \
   python pipeline_predict.py
--- tensorflow/mnist-gpu/model/pipeline_train.sh
+++ tensorflow/mnist-gpu/model/pipeline_train.sh
@@ -1,7 +1,8 @@
-PIPELINE_MODEL_RUNTIME=tfserving \
-PIPELINE_MODEL_TYPE=tensorflow \
 PIPELINE_MODEL_NAME=mnist \
-PIPELINE_MODEL_TAG=cpu \
-PIPELINE_INPUT_PATH=../input/ \
-PIPELINE_OUTPUT_PATH=./pipeline_tfserving/ \
+PIPELINE_MODEL_TAG=gpu \
+PIPELINE_MODEL_TYPE=tensorflow \
+PIPELINE_MODEL_RUNTIME=tfserving \
+PIPELINE_MODEL_CHIP=gpu \
+PIPELINE_INPUT_PATH=../input \
+PIPELINE_OUTPUT_PATH=./pipeline_tfserving \
   python pipeline_train.py
