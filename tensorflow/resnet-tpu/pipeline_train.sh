b5d4d78a6bb5ec43093acbb22dbbc1769b894e45
cfregly <chris@fregly.com>
2018-03-21 22:31:43 -0700

    added resnet

---
+++ tensorflow/resnet-tpu/pipeline_train.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+pip install cloud-tpu-profiler==1.5.1
+
+capture_tpu_profile \
+    --tpu_name=$TPU_NAME \
+	--logdir=$MODEL_DIR &
+
+tensorboard \
+    --logdir=$MODEL_DIR &
+
+python resnet_main.py \
+        --master=grpc://10.0.101.2:8470 \
+     	--data_dir=gs://cloud-tpu-test-datasets/fake_imagenet \
+	    --model_dir=gs://tpu-demo/rom/output2 \
+	    --use_tpu=True 
