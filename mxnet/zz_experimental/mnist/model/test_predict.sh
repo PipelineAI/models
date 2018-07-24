240c574a3df8e6b965a00cc34c741c6665a97bf6
cfregly <chris@fregly.com>
2018-07-23 22:35:00 -0700

    removed unused scripts

--- mxnet/zz_experimental/mnist/model/test_predict.sh
+++
@@ -1,5 +0,0 @@
-conda env create -n scikit --file pipeline_conda_environment.yml
-source activate scikit
-
-PIPELINE_INPUT_PATH=../input/predict/test_request.json PIPELINE_RESOURCE_RUNTIME=python PIPELINE_RESOURCE_SUBTYPE=scikit PIPELINE_RESOURCE_NAME=linear PIPELINE_RESOURCE_TAG=a PIPELINE_RESOURCE_RUNTIME=python PIPELINE_RESOURCE_CHIP=cpu \
-   python test.py
